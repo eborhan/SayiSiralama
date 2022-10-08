038a107fa7f9a3bcd5a1750e417bdab3968a137f
eborhan <106884905+eborhan@users.noreply.github.com>
2022-10-08 13:12:10 +0300

    Update SayiSiralama.java

--- src/test/java/SayiSiralama.java
+++ src/test/java/SayiSiralama.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SayiSiralama {
     public static void main(String[] args) {
 
-        int a,b,c;
+        int a, b, c;
 
         Scanner input = new Scanner(System.in);
         System.out.print("İlk sayıyı giriniz: ");
@@ -14,27 +14,32 @@ public class SayiSiralama {
         c = input.nextInt();
 
 
-        if (a > b && a >c){
-            if (b > c){
+        if (a > b && a > c) {
+            if (b > c) {
                 System.out.println("a > b > c");
-            }else{
+            } else if (b == c) {
+                System.out.println("a > b = c");
+            } else {
                 System.out.println("a > c > b");
             }
-        }else if (b > a && b > c){
-            if (a > c){
+        } else if (b > a && b > c) {
+            if (a > c) {
                 System.out.println("b > a > c");
-            }else{
+            } else if (a == c) {
+                System.out.println("b > a = c");
+            } else {
                 System.out.println("b > c > a");
             }
-        }else if (c > a && c > b){
-            if (a >b){
+        } else if (c > a && c > b) {
+            if (a > b) {
                 System.out.println("c > a > b");
-            }else{
+            } else if (a == b) {
+                System.out.println("c > a = b");
+            } else {
                 System.out.println("c > b > a");
             }
-        }else
+        }else {
             System.out.println("Sayılar eşit");
-
-
+        }
     }
 }
